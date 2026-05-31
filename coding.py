249a2827d308454cc1e51bc1a0b488600837a3bf
srilekhyan <srilekhya.nittala@gmail.com>
2026-05-30 18:21:53 -0700

    Add files via upload

---
+++ coding.py
@@ -0,0 +1,34 @@
+'''
+-> The customer name 
+-> deposit amount 
+-> set the pin 
+----------------------------------
+Registration: 
+Enter Your name: 
+Enter Your deposit amount: 
+Set a PIN for your account: 
+-------------------------
+ATM: 
+Hi, Alekhya . Welcome to Bank of America. 
+
+Enter the amount : 
+Enter the pin : 
+
+----------------------------------
+Success: 
+500 withrdraw succesfull 
+Avaialble Balance: 500 
+
+------------------------------------
+Error:
+Invalid PIN 
+
+------------------------------------
+Error: 
+Invalid Amount
+
+Thinks to Remember: 
+1. Dont accept negative ammount for withdrawal 
+2. Dont allow customer to withdraw above his deposit 
+
+'''
